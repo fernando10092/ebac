513ddebc66869c935a4f60c04c3903dbf45771d3
Fernando de Lima Gomes <fernando100@windowslive.com>
2024-01-19 20:58:39 -0300

    Plugin

---
+++ AULA_10_jquery_plugin/main.js
@@ -0,0 +1,29 @@
+var contador = 0;
+$(document).ready(function () {
+    $("#btEnviar").on("click", function (event) {
+        event.preventDefault();
+
+        //Dados na Lista
+        var lista = []
+        contador += 1;
+        lista.push($('#idNome').val(), $('#idEmail').val(), $('#idTel').val(), $('#idCpf').val(), $('#idEndereco').val(), $('#idCep').val())
+        alert(lista)
+
+        //Limpando Campos
+        $('#idNome').val('')
+        $('#idEmail').val('')
+        $('#idTel').val('')
+        $('#idCpf').val('')
+        $('#idEndereco').val('')
+        $('#idCep').val('')
+
+        //
+        document.querySelector('ul').innerHTML += '<h2>'+contador+' - '+lista+'</h2>'
+
+    });
+
+    //Mascaras
+    $('#idTel').mask('(00) 00000-0000')
+    $('#idCpf').mask("000.000.000-00")
+    $('#idCep').mask('00000-000')
+})
