782094b99632f43450bd38468f31386f2c9a6cfe
Fernando Lima <fernando100@windowslive.com>
2024-05-07 23:02:14 -0300

    ajax

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-   
+
     //LINK DA API
     const endpoint = "https://api.github.com/users/fernando10092";
 
@@ -9,16 +9,20 @@ $(document).ready(function () {
         .then(x => x.json())
         //THEN
         .then(y => {
-            console.log(y.name);
+
+            console.log(y);
             //document.getElementById('nome').innerHTML = y.name;
             document.getElementById("avatar").src = y.avatar_url
             $(".profile-name").text(y.name);
-            $(".profile-username").text("@"+y.login);
+            $(".profile-username").text("@" + y.login);
             $("#repositorio").text(y.public_repos);
             $("#seguidores").text(y.followers);
             $("#seguindo").text(y.following);
+
         }
         ).catch(
-            alert("Erro na requisição")
-        );
+            function (error) {
+                alert(error);
+            }
+        )
 }); 
