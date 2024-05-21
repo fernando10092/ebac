4eee64bc4bc84d93b2fbb42d9d86e930a9f54e0f
Fernando Lima <fernando100@windowslive.com>
2024-05-20 23:24:44 -0300

    typescript

---
+++ AULA_26-typescript/main.js
@@ -0,0 +1,17 @@
+"use strict";
+//Utilizando o TypeScript e a tipagem escreva duas funções: uma função de multiplicação que deverá receber 
+//como argumentos dois números e retornar a multiplicação deles, e uma função de saudação que deverá receber 
+//um nome e retornar a concatenação “Olá “ + nome;
+//FUNCAÂO 1
+function multip(num1, num2) {
+    return num1 * num2;
+}
+;
+console.log(multip(10, 20));
+//FUNCAO 2
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(saudacao("Fernando"));
+//No repositório do curso crie uma branch chamada exercicio_ts e armaze o código TS escrito nesta branch, 
+//nos envie o link através da plataforma.
---
+++ AULA_26-typescript/main.ts
@@ -0,0 +1,25 @@
+//Utilizando o TypeScript e a tipagem escreva duas funções: uma função de multiplicação que deverá receber 
+//como argumentos dois números e retornar a multiplicação deles, e uma função de saudação que deverá receber 
+//um nome e retornar a concatenação “Olá “ + nome;
+
+
+//FUNCAÂO 1
+function multip (num1: number, num2: number): number{
+
+    return num1 * num2;
+
+};
+
+console.log(multip(10, 20));
+
+
+//FUNCAO 2
+function saudacao(nome: string): string{
+    return "Olá "+nome;
+}
+
+console.log(saudacao("Fernando"))
+
+
+//No repositório do curso crie uma branch chamada exercicio_ts e armaze o código TS escrito nesta branch, 
+//nos envie o link através da plataforma.
